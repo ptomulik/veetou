24f35226b7ac8b14b1f048a24277b3126fe0aedc
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-02-11 11:16:42 +0100

    2019.02.11 11:16

--- share/oracle/packages/veetou/merges/ux/ux_zajecia_cykli.sql
+++ share/oracle/packages/veetou/merges/ux/ux_zajecia_cykli.sql
@@ -4,7 +4,7 @@ USING
         WITH u AS
         (
             SELECT
-                  ss_j.job_uuid
+                  cs_j.job_uuid
                 , COALESCE(subject_map.map_subj_code, subjects.subj_code) prz_kod
                 , semesters.semester_code cdyd_kod
                 , classes_map.map_classes_type tzaj_kod
@@ -33,48 +33,49 @@ USING
                 , COUNT(sm_j.map_id) dbg_subject_mapped
                 , COUNT(cm_j.map_id) dbg_classes_mapped
 
-            FROM v2u_ko_subject_semesters_j ss_j
+            FROM v2u_ko_classes_semesters_j cs_j
             INNER JOIN v2u_ko_subjects subjects
-                ON (subjects.id = ss_j.subject_id AND
-                    subjects.job_uuid = ss_j.job_uuid)
+                ON (subjects.id = cs_j.subject_id AND
+                    subjects.job_uuid = cs_j.job_uuid)
             INNER JOIN v2u_ko_semesters semesters
-                ON (semesters.id = ss_j.semester_id AND
-                    semesters.job_uuid = ss_j.job_uuid)
+                ON (semesters.id = cs_j.semester_id AND
+                    semesters.job_uuid = cs_j.job_uuid)
+            LEFT JOIN v2u_ko_matched_zajcykl_j ma_j
+                ON (ma_j.subject_id = cs_j.subject_id AND
+                    ma_j.specialty_id = cs_j.specialty_id AND
+                    ma_j.semester_id = cs_j.semester_id AND
+                    ma_j.job_uuid = cs_j.job_uuid AND
+                    ma_j.classes_type = cs_j.classes_type)
+            LEFT JOIN v2u_ko_missing_zajcykl_j mi_j
+                ON (mi_j.subject_id = cs_j.subject_id AND
+                    mi_j.specialty_id = cs_j.specialty_id AND
+                    mi_j.semester_id = cs_j.semester_id AND
+                    mi_j.job_uuid = cs_j.job_uuid AND
+                    mi_j.classes_type = cs_j.classes_type)
             LEFT JOIN v2u_ko_classes_map_j cm_j
-                ON (cm_j.subject_id = ss_j.subject_id AND
-                    cm_j.specialty_id = ss_j.specialty_id AND
-                    cm_j.semester_id = ss_j.semester_id AND
-                    cm_j.job_uuid = ss_j.job_uuid AND
+                ON (cm_j.subject_id = cs_j.subject_id AND
+                    cm_j.specialty_id = cs_j.specialty_id AND
+                    cm_j.semester_id = cs_j.semester_id AND
+                    cm_j.job_uuid = cs_j.job_uuid AND
+                    cm_j.classes_type = cs_j.classes_type AND
                     cm_j.selected = 1)
             LEFT JOIN v2u_classes_map classes_map
                 ON (classes_map.id = cm_j.map_id)
-            LEFT JOIN v2u_ko_matched_zajcykl_j ma_j
-                ON (ma_j.subject_id = ss_j.subject_id AND
-                    ma_j.specialty_id = ss_j.specialty_id AND
-                    ma_j.semester_id = ss_j.semester_id AND
-                    ma_j.job_uuid = ss_j.job_uuid AND
-                    ma_j.classes_type = cm_j.classes_type)
-            LEFT JOIN v2u_ko_missing_zajcykl_j mi_j
-                ON (mi_j.subject_id = ss_j.subject_id AND
-                    mi_j.specialty_id = ss_j.specialty_id AND
-                    mi_j.semester_id = ss_j.semester_id AND
-                    mi_j.job_uuid = ss_j.job_uuid AND
-                    mi_j.classes_type = cm_j.classes_type)
             LEFT JOIN v2u_ko_subject_map_j sm_j
-                ON (sm_j.subject_id = ss_j.subject_id AND
-                    sm_j.specialty_id = ss_j.specialty_id AND
-                    sm_j.semester_id = ss_j.semester_id AND
-                    sm_j.job_uuid = ss_j.job_uuid AND
+                ON (sm_j.subject_id = cs_j.subject_id AND
+                    sm_j.specialty_id = cs_j.specialty_id AND
+                    sm_j.semester_id = cs_j.semester_id AND
+                    sm_j.job_uuid = cs_j.job_uuid AND
                     sm_j.selected = 1)
             LEFT JOIN v2u_subject_map subject_map
                 ON (subject_map.id = sm_j.map_id)
             LEFT JOIN v2u_ko_grades_j grades
-                ON (grades.subject_id = ss_j.subject_id AND
-                    grades.specialty_id = ss_j.specialty_id AND
-                    grades.semester_id = ss_j.semester_id AND
-                    grades.job_uuid = ss_j.job_uuid)
+                ON (grades.subject_id = cs_j.subject_id AND
+                    grades.specialty_id = cs_j.specialty_id AND
+                    grades.semester_id = cs_j.semester_id AND
+                    grades.job_uuid = cs_j.job_uuid)
             GROUP BY
-                  ss_j.job_uuid
+                  cs_j.job_uuid
                 , COALESCE(subject_map.map_subj_code, subjects.subj_code)
                 , semesters.semester_code
                 , classes_map.map_classes_type
