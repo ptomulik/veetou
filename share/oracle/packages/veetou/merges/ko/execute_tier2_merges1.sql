7c38a692e633e21de749ab94cd952b987e647f4a
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-05-10 13:22:09 +0200

    2019.05.10 13:22

---
+++ share/oracle/packages/veetou/merges/ko/execute_tier2_merges1.sql
@@ -0,0 +1,20 @@
+@@ko_students.sql;
+@@ko_student_sheets_j.sql;
+@@ko_specialties.sql;
+@@ko_specialty_sheets_j.sql;
+@@ko_semesters.sql;
+@@ko_semester_sheets_j.sql;
+@@ko_subjects.sql;
+@@ko_subject_trs_j.sql;
+
+@@ko_specialty_semesters_j.sql;
+@@ko_student_semesters_j.sql;
+@@ko_subject_semesters_j.sql;
+@@ko_classes_semesters_j.sql;
+@@ko_grades_j.sql;
+
+@@ko_specialty_map_j.sql;
+@@ko_subject_map_j.sql;
+@@ko_classes_map_j.sql;
+
+-- vim: set ft=sql ts=4 sw=4 et:
---
+++ share/oracle/packages/veetou/merges/ko/execute_tier2_merges2.sql
@@ -0,0 +1,24 @@
+-- THE ORDER IS IMPORTANT!
+@@ko_matched_etpos_j.sql;
+@@ko_matched_prgos_j.sql;
+@@ko_missing_etpos_j.sql;
+@@ko_missing_prgos_j.sql;
+@@ko_matched_przcykl_j.sql;
+@@ko_matched_zajcykl_j.sql;
+@@ko_matched_przedm_j.sql;
+@@ko_matched_pktprz_j.sql;
+@@ko_matched_protos_j.sql;
+@@ko_matched_trmpro_j.sql;
+@@ko_matched_oceny_j.sql;
+
+@@ko_missing_zajcykl_j.sql;
+@@ko_missing_przcykl_j.sql;
+@@ko_missing_przedm_j.sql;
+@@ko_missing_pktprz_j.sql;
+@@ko_missing_protos_j.sql;
+@@ko_missing_trmpro_j.sql;
+@@ko_missing_oceny_j.sql;
+
+@@ko_skipped_programs_j.sql;
+
+-- vim: set ft=sql ts=4 sw=4 et:
